3c1d83bcb7f0de4be68f63858f2907789ac7d344
Dietmar Darimont <dietmar.darimont@gmail.com>
2024-01-16 00:49:56 +0100

    Update

--- src/main/java/JsonSerializer.java
+++ src/main/java/JsonSerializer.java
@@ -1,30 +1,42 @@
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 class JsonSerializer {
     public String serialize(Object object) {
-
         Class<?> clazz = object.getClass();
-        StringBuilder json = new StringBuilder("{");
-
         Field[] fields = clazz.getDeclaredFields();
+        Map<String, Object> fieldMap = new HashMap<>();
+
         for (Field field : fields) {
             try {
                 field.setAccessible(true);
                 Object value = field.get(object);
-                json.append("\"").append(field.getName()).append("\":\"").append(value).append("\",");
+                fieldMap.put(field.getName(), value);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
         }
 
-        json.deleteCharAt(json.length() - 1);
-        json.append("}");
+        return mapToJson(fieldMap);
+    }
 
+    private String mapToJson(Map<String, Object> map) {
+        StringBuilder json = new StringBuilder("{");
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            json.append("\"").append(entry.getKey()).append("\":\"").append(entry.getValue()).append("\",");
+        }
+
+        if (json.length() > 1) {
+            json.deleteCharAt(json.length() - 1);
+        }
+
+        json.append("}");
         return json.toString();
     }
 
     public static void main(String[] args) {
-        Mensch celine = new Mensch(32, "Celine", 3000000);
+        Mensch celine = new Mensch(32, "Celine");
         JsonSerializer serializer = new JsonSerializer();
         String json = serializer.serialize(celine);
         System.out.println(json);
@@ -34,27 +46,21 @@ class JsonSerializer {
 class Mensch {
     private int alter;
     private String name;
-    private int geld;
 
-    public Mensch(int alter, String name, int geld) {
+    public Mensch(int alter, String name) {
         this.alter = alter;
         this.name = name;
-        this.geld = geld;
     }
 
     public int getAlter() { return alter; }
     public void setAlter(int alter) { this.alter = alter; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-    public int getGeld() { return geld; }
-    public void setGeld(int geld) { this.geld = geld; }
 
     @Override
     public String toString() {
         return "Mensch{" +
                 "alter=" + alter +
-                ", name='" + name + '\'' +
-                ", geld=" + geld +
-                '}';
+                ", name='" + name + '}';
     }
 }
